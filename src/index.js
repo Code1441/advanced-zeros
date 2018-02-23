c4122880b5be76ab0c79751be16b1fd92e26faf1
Coder <31937370+Code1441@users.noreply.github.com>
2018-02-23 10:14:03 +0300

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+     var i = 1, count = 0;
+     var simpleDel = [], del = 2;
+     var numberDel = base;
+     while(numberDel>1){
+         if(numberDel%del==0) {
+             simpleDel.push(del);
+             numberDel /=del;
+         }
+         else del ++;
+     }
+     var mainDel = simpleDel[simpleDel.length-1];
+     var stepen = 0;
+     simpleDel.forEach(function(el){
+         if(el == mainDel) stepen++;
+     });
+      while(Math.pow(mainDel,i)<number){
+          count+=Math.floor(number /Math.pow(mainDel,i));
+          i++;
+      }
+      return Math.floor(count/stepen);
 }
